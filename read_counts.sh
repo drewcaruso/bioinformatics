3ec45802cde860e99c5a5a40b317269e4b8fd728
drewcaruso <drewcaruso213@gmail.com>
2019-11-08 10:41:46 -0500

    Update read_counts.sh

--- read_counts.sh
+++ read_counts.sh
@@ -8,8 +8,8 @@ echo "Sample,Reads" >> ${DEMUXED_DATA}/read_count.csv
 
 #loop iterating all read 1 files in folder
 for f in ${DEMUXED_DATA}/*R1_001*; do
-          #opening file, counting total numbers of lines divided by 4
-					CMD="$(zcat $f | echo $((`wc -l`/4)))"
-          #printing only fastq file name, not absolute path, read counts to read_count.csv
-  				echo "${f##*/},$CMD" >> ${DEMUXED_DATA}/read_count.csv
+	#opening file, counting total numbers of lines divided by 4
+	CMD="$(zcat $f | echo $((`wc -l`/4)))"
+        #printing only fastq file name, not absolute path, read counts to read_count.csv
+  	echo "${f##*/},$CMD" >> ${DEMUXED_DATA}/read_count.csv
 done
