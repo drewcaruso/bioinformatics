4c466eaae5149528f87b33b49cf862428415057e
drewcaruso <drewcaruso213@gmail.com>
2019-11-08 10:29:58 -0500

    Create read_counts.sh

---
+++ read_counts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#location of demultiplexed fastq files 
+DEMUXED_DATA="/path/to/fastq/files/"
+
+#creates CSV file to populate                              
+echo "Sample,Reads" >> ${DEMUXED_DATA}/read_count.csv
+
+#loop iterating all read 1 files in folder
+for f in ${DEMUXED_DATA}/*R1_001*; do
+          #opening file, counting total numbers of lines divided by 4
+					CMD="$(zcat $f | echo $((`wc -l`/4)))"
+          #printing only fastq file name, not absolute path, read counts to read_count.csv
+  				echo "${f##*/},$CMD" >> ${DEMUXED_DATA}/read_count.csv
+done
